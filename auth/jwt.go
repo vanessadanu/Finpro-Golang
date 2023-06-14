3f3785fe985757b6dfb2a7be6dc2a26388b389e4
Vanessa Danuwijaya <vanessa.danuwijaya@binus.ac.id>
2023-06-14 08:12:00 +0700

    jwt

--- auth/jwt.go
+++ auth/jwt.go
@@ -14,12 +14,12 @@ type BlacklistedToken struct {
 	ExpiresAt time.Time
 }
 
-var jwtSecret = []byte("secret-key") // Replace with your own secret key
+var jwtSecret = []byte("secret-key")
 
 func GenerateToken() (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(time.Hour).Unix()
+	claims["exp"] = time.Now().Add(10 * time.Minute).Unix()
 	tokenString, err := token.SignedString(jwtSecret)
 	if err != nil {
 		panic(err.Error())
@@ -45,19 +45,16 @@ func VerifyToken(tokenString string) (jwt.MapClaims, error) {
 }
 
 func ExtractToken(authorizationHeader string) string {
-	// Check if the authorization header is in the correct format
 	if !strings.HasPrefix(authorizationHeader, "Bearer ") {
 		return ""
 	}
 
-	// Extract the token from the authorization header
 	token := strings.TrimPrefix(authorizationHeader, "Bearer ")
 
 	return token
 }
 
 func AddToBlacklist(tokenString string) error {
-	// Parse the token to get the expiration time
 	token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
 	if err != nil {
 		return err
@@ -65,13 +62,11 @@ func AddToBlacklist(tokenString string) error {
 
 	expirationTime := time.Unix(int64(token.Claims.(jwt.MapClaims)["exp"].(float64)), 0)
 
-	// Create a new BlacklistedToken instance
 	blacklistedToken := BlacklistedToken{
 		Token:     tokenString,
 		ExpiresAt: expirationTime,
 	}
 
-	// Add the blacklisted token to the database
 	err = database.DB.Create(&blacklistedToken).Error
 	if err != nil {
 		return err
@@ -81,7 +76,6 @@ func AddToBlacklist(tokenString string) error {
 }
 
 func IsTokenBlacklisted(tokenString string) bool {
-	// Query the database to check if the token exists in the blacklist
 	var count int64
 	database.DB.Model(&BlacklistedToken{}).Where("token = ?", tokenString).Count(&count)
 
